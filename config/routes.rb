db9796e5eeabdc03298b5423a525d560aa74e8a8
Travis Arnold <ftntravis@gmail.com>
2014-04-06 16:02:43 -0700

    Updated show, view, added glyphicons, and changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Pinteresting::Application.routes.draw do
 
   devise_for :users
   
-  root "pages#home"
+  root "pins#index"
   
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
